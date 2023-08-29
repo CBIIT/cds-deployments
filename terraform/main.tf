ed50f44a965a22ec7e0c139c91cdd8a6bbb56ff9
cloudprofessionals <mycloudprofessionals@gmail.com>
2023-08-29 14:03:18 -0400

    fixing cds terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -44,15 +44,15 @@ module "ecs" {
 module "ecr" {
    count = var.create_ecr_repos ? 1: 0
    source = "git::https://github.com/CBIIT/datacommons-devops.git//terraform/modules/ecr?ref=v1.8"
-   stack_name = var.stack_name
    ecr_repo_names = var.ecr_repo_names
    tags = var.tags
-   create_env_specific_repo = var.create_env_specific_repo
    env = terraform.workspace
    enable_ecr_replication = var.enable_ecr_replication
    replication_destination_registry_id = var.replication_destination_registry_id
    replication_source_registry_id = var.replication_source_registry_id 
    allow_ecr_replication = var.allow_ecr_replication
+   resource_prefix = var.resource_prefix
+   project = var.resource_prefix
 }
 
 #create opensearch
