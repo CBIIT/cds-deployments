83e74363143b1cca6669458707244db19792620b
cloudprofessionals <mycloudprofessionals@gmail.com>
2023-08-30 15:27:01 -0400

    more changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -103,7 +103,6 @@ module "neo4j" {
   database_instance_type = var.database_instance_type
   tags = var.tags
   database_name = var.neo4j_database_name
-  resource_prefix = var.resource_prefix
 }
 
 
