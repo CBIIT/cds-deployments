595c2f44508a0eb3bbad3fa0f08e8c0f9c41366c
Fleming, Michael (NIH/NCI) [C] <michael.fleming@nih.gov>
2024-04-10 17:44:51 -0400

    updated hosts file settings

--- jenkins/jobs/DataDump.groovy
+++ jenkins/jobs/DataDump.groovy
@@ -74,7 +74,7 @@ pipeline {
 			    ansiblePlaybook( 
                 //playbook: '${WORKSPACE}/ansible/data-dump.yml',
                 playbook: '${WORKSPACE}/ansible/playbooks/data-dump.yml',
-                inventory: '${WORKSPACE}/inventory/hosts',
+                inventory: '${WORKSPACE}/inventory/hosts-db',
                 // extraVars: [
                 //   tier: "${params.Environment}",
 						    //   project_name: "${PROJECT}",
