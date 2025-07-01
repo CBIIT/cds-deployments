7a3c4a82c1ff1be14c6183437ca524099e4f5de0
cloudprofessionals <mycloudprofessionals@gmail.com>
2025-07-01 14:15:47 -0400

    add stack_name back

--- terraform/main.tf
+++ terraform/main.tf
@@ -99,7 +99,7 @@ module "neo4j" {
   db_subnet_id = var.db_subnet_id
   db_instance_volume_size = var.db_instance_volume_size
   public_ssh_key_ssm_parameter_name = var.public_ssh_key_ssm_parameter_name
-  # stack_name = var.stack_name
+  stack_name = var.stack_name
   db_private_ip = var.db_private_ip
   database_instance_type = var.database_instance_type
   tags = var.tags
